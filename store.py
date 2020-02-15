e498cdbb781c5dd8caaa7edaf2ba4a0d103456be
Martin Keegan <martin@no.ucant.org>
2020-02-15 13:18:23 +0000

    add store

---
+++ store.py
@@ -0,0 +1,10 @@
+
+# event data is parsed from JSON
+
+class Event(object):
+    def __init__(self, whatever):
+        pass
+
+def transform(event_data):
+    # this should return a list or generator of instances of Event()
+    pass
