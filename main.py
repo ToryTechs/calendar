6365d5815416fb2aa5a9a5edb8e44c386da5d715
Martin Keegan <martin@no.ucant.org>
2020-02-15 13:18:26 +0000

    add main

---
+++ main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import scrape
+import create_ics
+import store
+
+def run():
+    event_data = scrape.get_all_calendar_data()
+    events = store.transform(event_data)
+    create_ice(filename, events)
+
+if __name__ == '__main__':
+    run()
