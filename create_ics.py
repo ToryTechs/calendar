e70a983f899506f6828aed453bbf175135e4bbb1
Martin Keegan <martin@no.ucant.org>
2020-02-15 13:09:52 +0000

    add calendar generation

---
+++ create_ics.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+from ics import Calendar, Event
+
+def run():
+    c = Calendar()
+
+    e = Event()
+    e.name = "Parliamentary calendar test event"
+    e.begin = '2020-02-15 16:00:00'
+
+    c.events.add(e)
+
+    e = Event()
+    e.name = "A second test event"
+    e.begin = '2020-02-15 16:30:00'
+
+    c.events.add(e)
+
+    # c.events
+    path = 'parliament-test.ics'
+    with open(path, 'w') as my_file:
+        my_file.writelines(c)
+
+if __name__ == '__main__':
+    run()
+
+
+    # [<Event 'My cool event' begin:2014-01-01 00:00:00 end:2014-01-01 00:00:01>]
