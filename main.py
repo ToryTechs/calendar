0e0e102ac045ae7cf0e4bea621f7178032d865d4
Martin Keegan <martin@no.ucant.org>
2020-02-15 15:04:28 +0000

    add test mode

--- main.py
+++ main.py
@@ -3,11 +3,19 @@
 import scrape
 import create_ics
 import store
+import sys
 
 def run():
+    test_mode = "--fake-data" in sys.argv
+
     event_data = scrape.get_all_calendar_data()
-    events = store.transform(event_data)
-    create_ice.make(filename, events)
+
+    if test_mode:
+        events = store.transform(store.fakeData())
+    else:
+        events = store.transform(event_data)
+
+    create_ics.make(filename, events)
 
 if __name__ == '__main__':
     run()
